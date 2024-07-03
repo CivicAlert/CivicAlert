14794bc02a9edf0462a96bf78cd9d1d8d8380a5a
CivicAlert <Civicalert300@gmail.com>
2024-07-03 22:46:30 +0200

    updateDetailsUserProfile

--- app/src/main/java/com/example/civicalertoriginal/Screens/UpdateProfile.kt
+++ app/src/main/java/com/example/civicalertoriginal/Screens/UpdateProfile.kt
@@ -1,17 +1,12 @@
 package com.example.civicalertoriginal.Screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Call
 import androidx.compose.material.icons.rounded.Edit
@@ -20,13 +15,13 @@ import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.List
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -41,6 +36,17 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.civicalertoriginal.Components.BottomButtonsMyProfile
 import com.example.civicalertoriginal.Components.ProfileText
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+data class getUser(
+    val email: String = "",
+    val firstName: String = "",
+    val lastName: String = "",
+    val phoneNumber: String = ""
+)
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -76,6 +82,28 @@ fun UpdateProfile (navController: NavController){
                 }
             }
         }){innerPadding ->
+        }
+        var name by remember {
+            mutableStateOf("")
+        }
+        var sirname by remember {
+            mutableStateOf("")
+        }
+        var phoneNumber by remember {
+            mutableStateOf("")
+        }
+        var email by remember {
+            mutableStateOf("")
+        }
+            LaunchedEffect(Unit) {
+                getUserDetails (onUserFetched = { user ->
+                    name = user.firstName
+                    sirname  = user.lastName
+                    email= user.email
+                    phoneNumber = user.phoneNumber
+
+                })
+            }
             Column {
                 Spacer(modifier = Modifier.size(10.dp))
                 Column ( modifier = Modifier.fillMaxWidth() ,
@@ -83,35 +111,20 @@ fun UpdateProfile (navController: NavController){
                     Icon(imageVector = Icons.Rounded.Face, contentDescription = "", modifier = Modifier.size(80.dp
                     ))
                     Text(text = "USER NAME", fontSize = 25.sp)
-                }
-                val cardColor by remember {
-                    mutableStateOf(Color.Green)
-                }
-                var name by remember {
-                    mutableStateOf("")
-                }
-                var sirname by remember {
-                    mutableStateOf("")
-                }
-                var phoneNumber by remember {
-                    mutableStateOf("")
-                }
-                var email by remember {
-                    mutableStateOf("")
-                }
-
+                    Spacer(modifier = Modifier.size(10.dp))
                 Card (   modifier = Modifier
                     .fillMaxWidth(),
                     colors = CardDefaults.cardColors(containerColor = Color.White),
                     elevation = CardDefaults.cardElevation(defaultElevation = 12.dp) ){
                     Column {
-                        ProfileText(description = "First Name", value = "User name", onSave = {updateName -> name = updateName})
+                        ProfileText(description = "First Name", value = name, onSave = {updateName -> name = updateName})
+                        //Text(text = "Halo $name")
                         Spacer(modifier = Modifier.size(10.dp))
-                        ProfileText(description = "Last Name", value = "Sirname", onSave = {updateLastName -> sirname = updateLastName})
+                        ProfileText(description = "Last Name", value = sirname, onSave = {updateLastName -> sirname = updateLastName})
                         Spacer(modifier = Modifier.size(10.dp))
-                        ProfileText(description = "Email address", value = "emailInForm@gmail.com", onSave = {updatePhone -> email = updatePhone})
+                        ProfileText(description = "Email address", value = email, onSave = {updatePhone -> email = updatePhone})
                         Spacer(modifier = Modifier.size(10.dp))
-                        ProfileText(description = "Phone number", value = "+27 68801025", onSave = {updatePhone -> phoneNumber = updatePhone})
+                        ProfileText(description = "Phone number", value = phoneNumber, onSave = {updatePhone -> phoneNumber = updatePhone})
 
                     } }
 
@@ -122,10 +135,29 @@ fun UpdateProfile (navController: NavController){
             }
         }
 
-        
-        
+
+
     }
 }
+fun getUserDetails(onUserFetched:(getUser)-> Unit){
+    val database = FirebaseDatabase.getInstance()
+    val myRef = database.getReference("Community members")
+    val postListener = object : ValueEventListener{
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val user = snapshot.getValue(getUser::class.java)
+            if (user != null){
+                onUserFetched(user)
+            } else{
+                println("User null")
+            }
+        }
+
+
+        override fun onCancelled(error: DatabaseError) {
+            println("Failed to read value: ${error.toException()}")
+        } }
+    myRef.addValueEventListener(postListener)}
+
 @Preview
 @Composable
 fun SignUpPreview(){
